f43124a07ce7f398fd20e9b9a3dd17768434acde
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-02-17 16:49:13 +0000

    exercise 3

---
+++ range.py
@@ -0,0 +1,17 @@
+# def meter():
+# x = range(6)
+# for n in x:
+#     print(n)
+
+def meter():
+    i = 0
+    b = 10
+    liste=[]
+    while i < b:
+        i += 1
+        liste.append(i) 
+    print(liste)     
+
+meter()        
+
+
